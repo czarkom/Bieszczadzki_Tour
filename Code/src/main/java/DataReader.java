982eb794f02645a48674b17add59695014132d4c
czarkom <mczarkowski@onet.pl>
2019-11-24 21:37:09 +0100

    add line parsing and resources files

--- Code/src/main/java/DataReader.java
+++ Code/src/main/java/DataReader.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DataReader {
     private String dataFile;
@@ -12,9 +14,11 @@ public class DataReader {
     private BufferedReader reader1;
     private BufferedReader reader2;
 
-    public DataReader(String file1, String argument, String file2 ){
+    private ArrayList<Place> listOfPlaces = new ArrayList<>();
+
+    public DataReader(String file1, String file2 ){
         this.dataFile = file1;
-        this.startPlace = argument;
+        //this.startPlace = argument;
         this.wishlist = file2;
         getConfig();
     }
@@ -46,11 +50,45 @@ public class DataReader {
     private void getData(){
         initiateDataReader();
         advance(reader1);
+
+        while (this.currentLine != null){
+            if (currentLine.equals("### Miejsca podróży") || currentLine.equals("")
+            || currentLine.equals("Lp. | ID_miejsca | Nazwa miejsca | Opis miejsca |")){
+                advance(reader1);
+                continue;
+            }
+            if (currentLine.equals("### Czas przejścia")){
+                getTimes();
+                break;
+            }
+            System.out.println(currentLine);
+            parseLineInDataFilePlaces();
+            advance(reader1);
+        }
+    }
+
+    private void getTimes() {
+        //System.out.println(currentLine);
+        while (this.currentLine != null){
+            if (currentLine.equals("### Czas przejścia")
+            || currentLine.equals("Lp. | ID_miejsca_początkowego (S) | ID_miejsca_końcowego (E) | Czas S -> E | Czas E -> S | Jednorazowa opłata za przejście trasą (zł) |")){
+                advance(reader1);
+                continue;
+            }
+            System.out.println(currentLine);
+            advance(reader1);
+        }
     }
 
     private void getWishList(){
         initiateWishListReader();
         advance(reader2);
+
+        while (this.currentLine != null){
+            System.out.println(currentLine);
+            advance(reader2);
+        }
+        System.out.println("Doszedłem");
     }
 
     private void getConfig(){
@@ -58,11 +96,27 @@ public class DataReader {
         getWishList();
     }
 
-    private void parseLineInDataFile(){
+    private void parseLineInDataFilePlaces(){
+        Place place = new Place();
+        String[] firstSplit = currentLine.split("\\|");
+        System.out.println(Arrays.asList(firstSplit));
+        System.out.println(firstSplit[0].trim());
+        String firstPart = "" + firstSplit[0].trim().charAt(0);
+        int placeNumericId = Integer.parseInt(firstPart) - 1;
+        System.out.println(placeNumericId);
+
+    }
+
+    private void parseLineInDataFileTimes(){
         String[] firstSplit = currentLine.split("|");
     }
 
     private void parseLineInWishListFile(){
         String[] firstSplit = currentLine.split("|");
+    }
+
+    public static void main(String[] args){
+        System.out.println(args[0]);
+        DataReader reader = new DataReader(args[0], args[1]);
     }
 }
--- Code/src/main/java/Place.java
+++ Code/src/main/java/Place.java
@@ -1,8 +1,49 @@
 import java.util.ArrayList;
 
 public class Place {
-    public String name;
-    public String id;
-    public String typeOfPlace;
-    public ArrayList<Place> listOfConnections;
+    private String name;
+    private String id;
+    private String typeOfPlace;
+    private int numericId;
+    private ArrayList<Place> listOfConnections;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setNumericId(int numericId) {
+        this.numericId = numericId;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setTypeOfPlace(String typeOfPlace){
+        this.typeOfPlace = typeOfPlace;
+    }
+
+    public void addPathTo(Place place){
+        listOfConnections.add(place);
+    }
+
+    public ArrayList<Place> getListOfConnections(){
+        return listOfConnections;
+    }
+
+    public String getTypeOfPlace() {
+        return typeOfPlace;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumericId() {
+        return numericId;
+    }
 }
